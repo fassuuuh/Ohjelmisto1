4225240d20f240d0db1373ecdee87949024933a2
faizabahadori <faieze.bahadori@metropolia.fi>
2024-08-26 14:13:16 +0300

    update new3

---
+++ moduuli3/teht.3.py
@@ -0,0 +1,21 @@
+sukupuoli = input("Anna sukupuolesi (nainen, mies) : ")
+
+if sukupuoli == "nainen":
+    hemoglobiini_arvo = float(input("Anna hemoglobiiniarvo (g/l): "))
+
+if (sukupuoli == "nainen" and  117 <= hemoglobiini_arvo <= 175 ):
+    print("Hemoglobiiniarvo on normaali")
+elif (sukupuoli == "nainen" and hemoglobiini_arvo >= 175 ):
+    print("Hemoglobiiniarvo on liian korkea")
+elif (sukupuoli == "nainen" and hemoglobiini_arvo <= 117 ):
+    print("Hemoglobiiniarvo on liian alhainen")
+
+if sukupuoli == "mies":
+    hemoglobiini_arvo = float(input("Anna hemoglobiiniarvo (g/l): "))
+
+if (sukupuoli == "mies" and  134 <= hemoglobiini_arvo <= 195 ):
+    print("Hemoglobiiniarvo on normaali")
+elif (sukupuoli == "mies" and hemoglobiini_arvo >= 195 ):
+    print("Hemoglobiiniarvo on liian korkea")
+elif (sukupuoli == "mies" and hemoglobiini_arvo <= 134 ):
+    print("Hemoglobiiniarvo on liian alhainen")
