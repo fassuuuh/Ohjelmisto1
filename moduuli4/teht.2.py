75332991ad9dc1810269a212064c003716ab0df4
faizabahadori <faieze.bahadori@metropolia.fi>
2024-08-27 13:11:30 +0300

    update.

--- moduuli4/teht.2.py
+++ moduuli4/teht.2.py
@@ -3,6 +3,6 @@ while True:
     if tuumamäärä < 0:
         break
     tuuma_sentteinä = tuumamäärä * 2.54
-    print(f"Tuumat senttimetreinä: {tuuma_sentteinä:.2f} cm ")
+    print(f"{tuuma_sentteinä:.2f} cm ")
 
-print("Toiminto lopetettu. Syötä positiivinen luku.")
+print("Toiminto lopetettu. Syötit negatiivisen luvun.")
