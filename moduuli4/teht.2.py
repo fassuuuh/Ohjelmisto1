e993a73feff156aaf71dc34b665943ccd2487fe9
faizabahadori <faieze.bahadori@metropolia.fi>
2024-08-27 13:02:31 +0300

    update.

--- moduuli4/teht.2.py
+++ moduuli4/teht.2.py
@@ -1,4 +1,8 @@
-tuumamäärä = input("Syötä tuumamäärä: ")
-while tuumamäärä > "0" :
-    tuuma_sentteinä = float(tuumamäärä) * 2.54
-    print(f"Tuumat senttimetreinä: {int(tuuma_sentteinä)}")
+while True:
+    tuumamäärä = float(input("Syötä tuumamäärä: "))
+    if tuumamäärä < 0:
+        break
+    tuuma_sentteinä = tuumamäärä * 2.54
+    print(f"Tuumat senttimetreinä: {tuuma_sentteinä:.2f} cm ")
+
+print("Toiminto lopetettu. Syötä positiivinen luku.")
