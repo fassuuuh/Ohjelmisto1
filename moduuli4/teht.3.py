f8798aa24473b57268559f3215ce834a9996823f
faizabahadori <faieze.bahadori@metropolia.fi>
2024-08-27 13:38:32 +0300

    update.

---
+++ moduuli4/teht.3.py
@@ -0,0 +1,9 @@
+while True:
+    luku = (input("Anna luku: "))
+    if luku != "":
+        print(luku)
+    if luku == " ":
+        print("Tyhjä merkkijono. Toiminnot lopetettu.")
+        break
+
+
