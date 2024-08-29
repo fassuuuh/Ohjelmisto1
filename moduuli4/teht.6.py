dc5fb9e1676e0cebe14abb3fd10bb17984a08397
faizabahadori <faieze.bahadori@metropolia.fi>
2024-08-29 10:00:18 +0300

    updatejee.

--- moduuli4/teht.6.py
+++ moduuli4/teht.6.py
@@ -7,7 +7,6 @@ while iterator < N:
     iterator += 1
     x = random.random() * 2 - 1
     y = random.uniform(-1,1)
-    # print(f"Satunnainen piste:  {x}, {y}")
     if x**2 + y**2 < 1:
         print("Osui ympyrän sisälle.")
         n = n + 1
