26a8b7da45a4ec0a9d476b8b623b0dce084d3a82
faizabahadori <faieze.bahadori@metropolia.fi>
2024-08-29 10:19:51 +0300

    updatejee.

--- moduuli4/teht.4.py
+++ moduuli4/teht.4.py
@@ -1,2 +1,14 @@
 import random
-random.randint
+luku = random.randint(1,10)
+
+while True:
+    arvaus = float(input("Syötä luku (1-10): "))
+
+    if arvaus > luku:
+        print("Liian suuri arvaus!")
+    if arvaus < luku:
+        print("Liian pieni arvaus!")
+    if arvaus == luku:
+        print("Oikein!")
+        break
+
