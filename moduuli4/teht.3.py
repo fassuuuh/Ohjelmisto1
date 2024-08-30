423fbd3c7a8ff6b2ca8502ac2b0b61b8d9bef9b5
faizabahadori <faieze.bahadori@metropolia.fi>
2024-08-30 13:37:32 +0300

    update.

--- moduuli4/teht.3.py
+++ moduuli4/teht.3.py
@@ -2,14 +2,18 @@ pienin = None
 suurin = None
 
 while True:
-    luku = float(input("Anna luku: "))
-    if pienin is None or luku < pienin:
-        pienin = luku
-    if suurin is None or luku > suurin:
-        suurin = luku
+    luku = (input("Anna luku: "))
 
     if luku == "":
         print(f"Pienin annettu luku: {pienin}")
         print(f"Suurin annettu luku: {suurin}")
         print("Lopetetaan toiminnot.")
         break
+
+    luku = int(luku)
+
+    if pienin is None or luku < pienin:
+        pienin = luku
+    if suurin is None or luku > suurin:
+        suurin = luku
+
