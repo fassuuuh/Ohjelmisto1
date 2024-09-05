61fe40e6ec8fb61ce4effdc836e9d873564795c5
faizabahadori <faieze.bahadori@metropolia.fi>
2024-09-05 19:27:18 +0300

    update

--- moduuli6/teht.3.py
+++ moduuli6/teht.3.py
@@ -6,8 +6,9 @@ def muunnos(gallon):
 while True:
     syote = input("Syötä gallonamäärä: ")
     syote = float(syote)
-    litroiksi = muunnos(syote)
-    print(f"Syöttämäsi gallonamäärä {syote:.1f} gal on {litroiksi:.2f} litraa.")
     if syote < 0:
         print("Negatiivinen luku syötetty. Ohjelma lopetetaan.")
         break
+    litroiksi = muunnos(syote)
+    print(f"Syöttämäsi gallonamäärä {syote:.1f} gal on {litroiksi:.2f} litraa.")
+
